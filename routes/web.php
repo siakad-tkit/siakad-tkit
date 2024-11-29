20b84a5fc393629118e74d35ebd52e2d9ac660b1
Hery Siswanto <hery505@gmail.com>
2024-11-30 05:09:17 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\AbsensiController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
